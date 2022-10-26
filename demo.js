43cd89284842a56e01cf514a14beec0ce4962925
rzhu1999 <rzhu08448@gmail.com>
2022-10-26 16:36:52 -0700

    [Fixed] Can not run template app

--- demo.js
+++ demo.js
@@ -1,12 +1,12 @@
-var express = require('express');
-const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const staple = require('staple-api');
 
-let ontology = {
+const ontology = {
     file: './docs/ontology.ttl',
 };
 
-let config = {
+const config = {
     dataSources: {
         default: 'defaultSource',
         defaultSource: {
@@ -19,20 +19,38 @@ let config = {
     },
 };
 
-async function StapleDemo() {
-    let stapleApi = await staple(ontology, config);
+async function Staple() {
+    const stapleApi = await staple(ontology, config);
+    const schema = stapleApi.schema;
 
-    var app = express();
-    app.use(
-        '/graphql',
-        graphqlHTTP({
-            schema: stapleApi.schema,
-            graphiql: true,
-        })
-    );
+    // creating the list of all people and their names for FE indexing
 
-    app.listen(4000);
-    console.log('Running a GraphQL API server at localhost:4000/graphql');
+    let people = [];
+
+    console.log("Fetching people's names...");
+    await stapleApi.graphql('{ Person { _id label } }').then((response) => {
+        response.data.Person.forEach((element) => {
+            people.push({ id: element._id, text: element.label });
+        });
+        console.log('...all fetched!');
+    });
+
+    const app = express();
+    app.use(express.static('docs'));
+
+    app.get('/people', function (req, res) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.send(people);
+    });
+
+    const server = new ApolloServer({
+        schema,
+    });
+
+    const path = '/graphql';
+    server.applyMiddleware({ app, path });
+
+    app.listen({ port: 3000 }, () => console.log('🚀 Server ready'));
 }
 
-StapleDemo();
+Staple();
